9d8f41405e870ad0cc5afa5729b184883e02c7b3
elinor233 <elinorshaar233@gmail.com>
2023-09-08 13:54:16 +0300

    Adding a table configuration and changing the design of the report form

--- cost_manager/src/ReportForm.js
+++ cost_manager/src/ReportForm.js
@@ -6,12 +6,14 @@ const ReportForm = () => {
   const [month, setMonth] = useState(new Date().getMonth());
   const [year, setYear] = useState(new Date().getFullYear());
   const [items, setItems] = useState([]);
+  const [showTable, setShowTable] = useState(false); 
 
   const fetchReport = async () => {
     try {
       const db = await openCostsDB("costsDB");
       const fetchedItems = await db.getMonthlyReport(month, year);
       setItems(fetchedItems);
+      setShowTable(true); 
     } catch (err) {
       alert("Error fetching report");
     }
@@ -22,30 +24,43 @@ const ReportForm = () => {
       <h2>Monthly Report</h2>
       <div>
         <label>Month:</label>
-        <input 
-          type="number" 
-          min="1" 
-          max="12" 
-          value={month + 1} 
-          onChange={(e) => setMonth(parseInt(e.target.value) - 1)} 
+        <input
+          type="number"
+          min="1"
+          max="12"
+          value={month + 1}
+          onChange={(e) => setMonth(parseInt(e.target.value) - 1)}
         />
       </div>
       <div>
         <label>Year:</label>
-        <input 
-          type="number" 
-          value={year} 
-          onChange={(e) => setYear(parseInt(e.target.value))} 
+        <input
+          type="number"
+          value={year}
+          onChange={(e) => setYear(parseInt(e.target.value))}
         />
       </div>
       <button onClick={fetchReport}>Fetch Report</button>
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>
-            {item.description} - {item.sum} ({item.category})
-          </li>
-        ))}
-      </ul>
+      {showTable && (
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Sum</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td>{item.description}</td>
+                <td>{item.sum}</td>
+                <td>{item.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
